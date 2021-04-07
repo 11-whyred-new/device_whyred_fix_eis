98eef3cbc1e087cb91f477ef7fa10ea39c1644f9
root <root@iqbal.heymnpl2vh0ujozbrymtoeynvb.cwx.internal.cloudapp.net>
2021-04-07 10:36:43 +0000

    sq lite

--- device.mk
+++ device.mk
@@ -165,8 +165,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
-    libxml2 \
-    Snap
+    libxml2
 
 # CNE
 PRODUCT_PACKAGES += \
