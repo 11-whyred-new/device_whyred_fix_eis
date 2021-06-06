3c9b8f2ba5b52f9b703a676d02dc38786de056c4
Ubuntu <raphael@raphael.ehp0n55wvzhu3m2fz1tduxiosh.lx.internal.cloudapp.net>
2021-06-06 14:47:52 +0000

    wfd

--- device.mk
+++ device.mk
@@ -208,8 +208,10 @@ PRODUCT_PACKAGES += \
     gralloc.sdm660 \
     hwcomposer.sdm660 \
     libdisplayconfig \
+    libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
     liboverlay \
-    libqdMetaData.system \
+    libqdMetaData.vendor \
     libtinyxml \
     memtrack.sdm660 \
     vendor.display.config@1.9 \
@@ -524,7 +526,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libaacwrapper \
     libmediaextractorservice \
-    libnl
+    libnl \
+    libstagefright_enc_common
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
